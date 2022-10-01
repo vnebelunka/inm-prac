b6435d1e9ebd94afa019243abedc3b129445f077
vnebelunka <vnebelunka@gmail.com>
2022-10-01 21:47:45 +0300

    rotation matrix generation move to check function (now it is sequence of cos / sin) rotations

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ using namespace std::chrono;
  * @param s sin of rotation
  * @param start first != 0 element of rows.
  */
-void rotationRow(Matrix<double> &A, int i, int j, double c, double s, int start = 0){
+void rotationRow(Matrix<double> &A, int i, int j, double c, double s, int start = 0, int stop = -1){
     size_t n = A.size().first;
-    for(int k = 0; k < n; ++k){
+    for(int k = start; k < n; ++k){
         double aik = c * A(i, k) - s * A(j, k);
         double ajk = s * A(i, k) + c * A(j, k);
         A(i, k) = aik, A(j, k) = ajk;
@@ -50,22 +50,24 @@ void rotationCol(Matrix<double> &A, int i, int j, double c, double s, int start 
  * Tridiagonalize of original matrix A.
  * @param A original matrix.
  */
-void Tridiagonaliation(Matrix<double> const &A, Matrix<double>  &D, Matrix<double>  &Q) {
+void Tridiagonaliation(Matrix<double> const &A, Matrix<double> &D, vector<std::pair<double, double>> &rots) {
     size_t n = A.size().first;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             D(i, j) = A(i, j);
-            Q(i, j) = i == j;
         }
     }
+
+    rots.reserve(n * n);
+
     for (int i = 0; i < n; ++i) {
-        for (int j = i + 2; j < n; ++j) {
-            double mod = sqrt(D(i + 1, i) * D(i + 1, i) + D(j, i) * D(j, i));
-            double s = -D(j, i) / mod, c = D(i + 1, i) / mod;
-            rotationRow(D, i + 1, j, c, s, i);
-            rotationRow(Q, i + 1, j, c, s, 0);
-            rotationCol(D, i + 1, j, c, s, i);
-        }
+            for (int j = i + 2; j < n; ++j) {
+                double mod = sqrt(D(i + 1, i) * D(i + 1, i) + D(j, i) * D(j, i));
+                double s = -D(j, i) / mod, c = D(i + 1, i) / mod;
+                rotationRow(D, i + 1, j, c, s, i);
+                rotationCol(D, i + 1, j, c, s, i);
+                rots.emplace_back(c, s);
+            }
     }
 }
 
@@ -75,9 +77,24 @@ void Tridiagonaliation(Matrix<double> const &A, Matrix<double>  &D, Matrix<doubl
  * @param D
  * @param Q
  */
-void check(Matrix<double> const &A, Matrix<double> const &D, Matrix<double> const &Q){
+void check(Matrix<double> const &A, Matrix<double> const &D, vector<std::pair<double, double>> &rots){
     size_t n = A.size().first;
     Matrix<double> C(n, n), F(n, n);
+    Matrix<double> Q(n, n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            Q(i, j) = i == j;
+        }
+    }
+    int pos = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 2; j < n; ++j) {
+            double c = rots[pos].first, s = rots[pos++].second;
+            rotationRow(Q, i + 1, j, c, s, 0);
+        }
+    }
+
+
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
             for(int k = 0; k < n; ++k){
@@ -94,7 +111,7 @@ void check(Matrix<double> const &A, Matrix<double> const &D, Matrix<double> cons
     }
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
-            if(fabs(A(i, j) - F(i, j)) >= 1e-12){
+            if(fabs(A(i, j) - F(i, j)) >= 1e-10){
                 std::cout << "Matrix factorization is incorrect!\n";
                 exit(1);
             }
@@ -111,7 +128,8 @@ int main() {
     std::default_random_engine eng(rd());
     std::uniform_real_distribution<double> distr(-100, 100);
 
-    Matrix<double> A(n, n), D(n, n), Q(n, n);
+    Matrix<double> A(n, n), D(n, n);
+    vector<std::pair<double, double>> rots;
     for(int i = 0; i < n; ++i){
         for(int j = i; j < n; ++j){
             A(i, j) = distr(eng);
@@ -119,11 +137,11 @@ int main() {
         }
     }
     auto start = high_resolution_clock::now();
-    Tridiagonaliation(A, D, Q);
+    Tridiagonaliation(A, D, rots);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
     std::cout <<std::endl<< "duration: "<< duration.count() << std::endl;
-    if(n <= 64) {
-        check(A, D, Q);
+    if(n <= 256) {
+        check(A, D, rots);
     }
 }
