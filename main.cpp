54f1765839d165d2a34d760388577143d2db2443
vnebelunka <vnebelunka@gmail.com>
2022-10-19 18:53:23 +0300

    cache optimization added (only row operations)

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ using namespace std::chrono;
  * @param s sin of rotation
  * @param start first != 0 element of rows.
  */
-void rotationRow(Matrix<double> &A, int i, int j, double c, double s, int start = 0, int stop = -1){
+void rotationRow(Matrix<double> &A, int i, int j, double c, double s, int start = 0){
     size_t n = A.size().first;
     for(int k = start; k < n; ++k){
         double aik = c * A(i, k) - s * A(j, k);
@@ -28,21 +28,25 @@ void rotationRow(Matrix<double> &A, int i, int j, double c, double s, int start 
     }
 }
 
-/**
- * Apply rotation matrix to A (rotation 'i' and 'j') cols
- * @param A original matrix
- * @param i first col
- * @param j second col
- * @param c cos of rotation
- * @param s sin of rotation
- * @param start first != 0 element of cols
- */
-void rotationCol(Matrix<double> &A, int i, int j, double c, double s, int start = 0){
-    size_t n = A.size().first;
-    for(int k = start; k < n; ++k){
-        double aki = c * A(k, i) - s * A(k, j);
-        double akj = s * A(k, i) + c * A(k, j);
-        A(k, i) = aki, A(k, j) = akj;
+void applyRotation(Matrix<double> & D, int row, vector<std::pair<double, double>> &rots){
+    size_t n = D.size().first;
+    int rot_start = rots.size();
+    //Apply rotation to rows: A -> QA
+    for (int j = row + 2; j < n; ++j) {
+        double mod = sqrt(D(row + 1, row) * D(row + 1, row) + D(j, row) * D(j, row));
+        double s = -D(j, row) / mod, c = D(row + 1, row) / mod;
+        rotationRow(D, row + 1, j, c, s, row);
+        rots.emplace_back(c, s);
+    }
+    //Apply rotation to cols QA -> QAQ^T
+    for(int i = row; i < n; ++i){
+        int cur_rot = rot_start;
+        for(int j = row + 2; j < n; ++j, ++cur_rot){
+            double c = rots[cur_rot].first, s = rots[cur_rot].second;
+            double aii = c * D(i, row + 1) - s * D(i, j);
+            double aij = s * D(i, row + 1) + c * D(i, j);
+            D(i, row + 1) = aii, D(i, j) = aij;
+        }
     }
 }
 
@@ -60,14 +64,8 @@ void Tridiagonaliation(Matrix<double> const &A, Matrix<double> &D, vector<std::p
 
     rots.reserve(n * n);
 
-    for (int i = 0; i < n; ++i) {
-            for (int j = i + 2; j < n; ++j) {
-                double mod = sqrt(D(i + 1, i) * D(i + 1, i) + D(j, i) * D(j, i));
-                double s = -D(j, i) / mod, c = D(i + 1, i) / mod;
-                rotationRow(D, i + 1, j, c, s, i);
-                rotationCol(D, i + 1, j, c, s, i);
-                rots.emplace_back(c, s);
-            }
+    for (int i = 0; i < n - 2; ++i) {
+        applyRotation(D, i, rots);
     }
 }
 
@@ -124,8 +122,8 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    std::random_device rd;
-    std::default_random_engine eng(rd());
+    //std::random_device rd;
+    std::default_random_engine eng;
     std::uniform_real_distribution<double> distr(-100, 100);
 
     Matrix<double> A(n, n), D(n, n);
