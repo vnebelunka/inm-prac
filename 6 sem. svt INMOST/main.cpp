f7c60793247cf7a4129fbca858cf2ae586bd8d04
vnebelunka <vnebelunka@gmail.com>
2023-03-10 19:46:27 +0300

    timer added

--- 6 sem. svt INMOST/main.cpp
+++ 6 sem. svt INMOST/main.cpp
@@ -3,16 +3,32 @@
 
 using namespace INMOST;
 
-int main(){
+int main(int argc, char**argv){
+    Solver::Initialize(&argc, &argv);
     Sparse::Matrix A;
-    A.Load("/home/vnebelunka/Загрузки/2/A_tet1.mtx");
+    double tempTimer, solvingTimer;
+    bool success;
+
+    char *matrix_fname = argv[1], *rhs_fname = argv[2], *drop_tol = argv[3];
+
+    A.Load(matrix_fname);
     Sparse::Vector rhs;
-    rhs.Load("/home/vnebelunka/Загрузки/2/rhs_tet1.mtx");
+    rhs.Load(rhs_fname);
 
     Solver S("inner_ilu2");
-    S.SetParameter("verbosity", "3");
+
+    S.SetParameter("verbosity", "2");
+    S.SetParameter("drop_tolerance", drop_tol);
+
+    tempTimer = Timer();
     S.SetMatrix(A);
+    std::cout << "preconditioner time: " << Timer() - tempTimer << std::endl;
     Sparse::Vector sol(rhs);
-    std ::cout  << S.Solve(sol, rhs) << std::endl;
+    tempTimer = Timer();
+    success = S.Solve(rhs, sol);
+    solvingTimer = Timer() - tempTimer;
+    std::cout << "solving time: " << solvingTimer << std::endl;
+    std::cout << "iterations: " << S.Iterations() << std::endl;
+    Solver::Finalize();
     return 0;
 }
