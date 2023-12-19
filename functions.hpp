4d408429b2a5971efb236ca26257e902fb80dceb
vnebelunka <vnebelunka@gmail.com>
2023-12-19 20:27:18 +0300

    initial commit

--- functions.hpp
+++ functions.hpp
@@ -3,16 +3,29 @@
 //
 #include <cmath>
 
-double dx = 7.75, dy = 3.25, dxy = 3.89711432;
+enum time_iteration_type
+{
+	EXPLICIT = 1,
+	IMPLICIT = 2
+};
 
-//double dx = 10, dy = 1, dxy = 0;
+const time_iteration_type Problem_type = EXPLICIT;
+const double dx = 7.75, dy = 3.25, dxy = 3.89711432;
+const double dt = 3e-4;
+const int T = 100;
 
-const double a = 10;
 
-double u(double x, double y){
-    return sin(a*x) * sin(a*y);
+double u0(double x, double y){
+    if(x > 0.35 && x < 0.65 && y > 0.35 && y < 0.65){
+        return 1;
+    }
+    return 0;
 }
 
-double f(double x, double y){
-    return -a*a * (2.*dxy * cos(a*x)*cos(a*y) - (dx+dy) * sin(a*x)*sin(a*y));
+double phi(double x, double y, double t){
+    return 0;
+}
+
+double f(double x, double y, double t){
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -7,16 +7,12 @@
 #include <iostream>
 
 #include "inmost.h"
+#include "functions.hpp"
 
 using namespace INMOST;
 
-extern double f(double x, double y);
-extern double g_bond(double x, double y);
 
-extern double u(double x, double y, double dx = 1., double dy = 1.);
-
-
-void save_solution(size_t n, const std::vector<double>& sol, const std::vector<double>& exact_sol, std::string vtk_fname){
+void save_solution(size_t n, const Sparse::Vector &sol, std::string vtk_fname){
     size_t N = n * n;
     double h = 1. / n;
     FILE* fd = fopen(vtk_fname.c_str(), "w");
@@ -38,100 +34,98 @@ void save_solution(size_t n, const std::vector<double>& sol, const std::vector<d
     for(int i = 0; i < N; ++i){
         fprintf(fd, "%lf\n", sol[i]);
     }
-    fputc('\n', fd);
-    if(!exact_sol.empty()) {
-        fprintf(fd, "SCALARS U_true double 1\n");
-        fprintf(fd, "LOOKUP_TABLE default\n");
-        for (int i = 0; i < N; ++i) {
-            fprintf(fd, "%lf\n", exact_sol[i]);
+}
+
+void calc_matrix(Sparse::Matrix& A, const size_t n, const double h){
+    size_t N = n * n;
+    A.SetInterval(0, N);
+    const double h2 = h * h;
+#pragma omp parallel for
+    for(int k = 0; k < N; ++k){
+        int i = k / n, j = k % n;
+        double x = h * i, y = h * j;
+        if(i == 0 || j == 0 || i == n - 1 || j == n - 1){ // boundary points
+            A[k][k] = 1;
+        } else {
+            A[k][k] = 2 * (dx + dy) / h2;
+            if(Problem_type == IMPLICIT){
+                A[k][k] += 1. / dt;
+            }
+            A[k][k + n] = -dx / h2;
+            A[k][k - n] = -dx / h2;
+            A[k][k - 1] = -dy / h2;
+            A[k][k + 1] = -dy / h2;
+            A[k][k + n + 1] = -dxy / (2 * h2);
+            A[k][k - n - 1] = -dxy / (2 * h2);
+            A[k][k - n + 1] = dxy / (2 * h2);
+            A[k][k + n - 1] = dxy / (2 * h2);
         }
-        fputc('\n', fd);
     }
 }
 
+
+void apply_iteration(Solver& S, const size_t n, double h, Sparse::Vector &u, int t_step, Sparse::Matrix& A){
+    size_t N = n * n;
+    if(Problem_type == IMPLICIT){
+        double t = t_step * dt;
+        for(int k = 0; k < N; ++k){
+            int i = k / n, j = k % n;
+            double x = h * i, y = h * j;
+            if(i == 0 || j == 0 || i == n - 1 || j == n - 1){ // boundary points
+                u[k] = phi(x, y, t);
+            } else {
+                u[k] /= dt;
+                u[k] += f(x, y, t);
+            }
+        }
+        bool solved = S.Solve(u, u);
+        if(!solved){
+            std::cout << "Linear solver failure!" << std::endl;
+            std::cout << "Reason: " << S.ReturnReason() << std::endl;
+        }
+    } else {
+        double t = (t_step - 1) * dt;
+        Sparse::Vector temp;
+        temp.SetInterval(0, N);
+        A.MatVec(1.0, u, 0.0, temp);
+        for(int k = 0; k < N; ++k){
+            int i = k / n, j = k % n;
+            double x = h * i, y = h * j;
+            u[k] = u[k] + (f(x, y, t) - temp[k]) * dt;
+        }
+    }
+    save_solution(n, u, std::string("res") + std::to_string(t_step) + std::string(".vtk"));
+}
 
 int main(int argc, char**argv){
     Solver::Initialize(&argc, &argv);
     int n = strtol(argv[1], nullptr, 10), N = n * n;
     double h = 1. / n;
     std::cout << "h: " << h << " n: " << n << std::endl;
-    double dx = 1.0, dy = 1.0;
-    if(argc > 3){
-        dx = strtod(argv[3], nullptr);
-    }
-    if(argc > 4){
-        dy = strtod(argv[4], nullptr);
-    }
+
     std::cout << "dx: " << dx << " dy: " << dy << std::endl;
 
     Sparse::Matrix A;
-    Sparse::Vector b;
-    Sparse::Vector sol;
-
-    A.SetInterval(0, N);
-    b.SetInterval(0, N);
-    sol.SetInterval(0, N);
-
+    calc_matrix(A, n, h);
+    Sparse::Vector u;
+   
+    u.SetInterval(0, N);
     for(int k = 0; k < N; ++k){
         int i = k / n, j = k % n;
         double x = h * i, y = h * j;
-        if(i == 0 || j == 0 || i == n - 1 || j == n - 1){ // boundary points
-            A[k][k] = 1;
-            b[k] = g_bond(x, y);
-        } else {
-            A[k][k] = 2 * (dx + dy);
-            A[k][k + n] = -dx;
-            A[k][k - n] = -dx;
-            A[k][k - 1] = -dy;
-            A[k][k + 1] = -dy;
-            b[k] = f(x, y) * h * h;
-        }
+        u[k] = u0(x, y);
     }
-
-    std::cout<< "matrix solver started\n";
+    save_solution(n, u, "res0.vtk");
 
     Solver S(Solver::INNER_ILU2);
     S.SetParameter("absolute_tolerance", "1e-12");
     S.SetParameter("verbosity", "2");
     S.SetParameter("relative_tolerance", "1e-7");
     S.SetMatrix(A);
-    bool solved = S.Solve(b, sol);
-    std::cout << "lin.it.: " << S.Iterations() << std::endl;
-    if(!solved){
-        std::cout << "Linear solver failure!" << std::endl;
-        std::cout << "Reason: " << S.ReturnReason() << std::endl;
+    
+    for(int t_step = 1; t_step <= T; ++t_step){
+        apply_iteration(S, n, h, u, t_step, A);
     }
-    std::cout << "matrix solver ended\n";
-
-    std::vector<double> full_sol(N);
-    for(int i = 0; i < N; ++i){
-        full_sol[i] = sol[i];
-    }
-
-    std::vector<double> exact_sol(N);
-    for(int k = 0; k < N; ++k){
-        int i = k / n, j = k % n;
-        exact_sol[k] = u(i * h, j * h, dx, dy);
-    }
-
-
-    std::string res_fname = "./res.vtk";
-    if(argc > 2){
-        res_fname = std::string(argv[2]);
-    }
-
-    double l2_norm = 0., c_norm = 0;
-    for(int i = 0; i < N; ++i){
-        l2_norm += (exact_sol[i] - sol[i]) * (exact_sol[i] - sol[i]);
-        c_norm = std::max(c_norm, abs(exact_sol[i] - sol[i]));
-    }
-    l2_norm = sqrt(l2_norm) / N;
-    std::cout << "L2 norm: " << l2_norm << " C-norm: " << c_norm << std::endl;
-
-
-    std::cout << "saving solution started with res in " << res_fname << " file\n";
-    save_solution(n, full_sol, exact_sol, res_fname);
-    std::cout << "saving solution ended\n";
 
     Solver::Finalize();
 }
